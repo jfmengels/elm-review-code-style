63abbb749d14beda206816bf819a4f0acb8b9277
Simon Lydell <simon.lydell@gmail.com>
2024-09-03 21:38:19 +0200

    Compiles

--- src/NoRedundantlyQualifiedType.elm
+++ src/NoRedundantlyQualifiedType.elm
@@ -6,12 +6,17 @@ module NoRedundantlyQualifiedType exposing (rule)
 
 -}
 
-import Elm.Syntax.Declaration as Declaration exposing (Declaration(..))
-import Elm.Syntax.Expression as Expression exposing (Expression, LetBlock, LetDeclaration(..))
+import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Exposing as Exposing exposing (Exposing)
+import Elm.Syntax.Expression as Expression exposing (Expression, Function, LetBlock, LetDeclaration(..))
+import Elm.Syntax.Import exposing (Import)
 import Elm.Syntax.Node as Node exposing (Node)
-import Elm.Syntax.TypeAnnotation exposing (TypeAnnotation(..))
+import Elm.Syntax.Range exposing (Range)
+import Elm.Syntax.TypeAnnotation exposing (RecordField, TypeAnnotation(..))
+import Review.Fix as Fix
 import Review.ModuleNameLookupTable as ModuleNameLookupTable exposing (ModuleNameLookupTable)
 import Review.Rule as Rule exposing (Rule)
+import Set exposing (Set)
 
 
 {-| Reports when a type is qualified by a module (alias) of the same name.
@@ -66,19 +71,53 @@ rule =
     Rule.newModuleRuleSchemaUsingContextCreator "NoRedundantlyQualifiedType" initialContext
         |> Rule.withDeclarationEnterVisitor declarationVisitor
         |> Rule.withLetDeclarationEnterVisitor letDeclarationEnterVisitor
-        |> Rule.withFinalModuleEvaluation finalEvaluation
         |> Rule.fromModuleRuleSchema
 
 
 type alias Context =
-    { lookupTable : ModuleNameLookupTable }
+    { lookupTable : ModuleNameLookupTable
+    , imports : List (Node Import)
+    , typesDefinedInModule : Set String
+    }
 
 
 initialContext : Rule.ContextCreator () Context
 initialContext =
     Rule.initContextCreator
-        (\lookupTable () -> { lookupTable = lookupTable })
+        (\lookupTable ast () ->
+            { lookupTable = lookupTable
+            , imports = ast.imports
+            , typesDefinedInModule = collectTypesDefinedInModule ast.declarations
+            }
+        )
         |> Rule.withModuleNameLookupTable
+        |> Rule.withFullAst
+
+
+collectTypesDefinedInModule : List (Node Declaration) -> Set String
+collectTypesDefinedInModule =
+    List.filterMap
+        (\node ->
+            case Node.value node of
+                FunctionDeclaration _ ->
+                    Nothing
+
+                AliasDeclaration typeAlias ->
+                    Just (Node.value typeAlias.name)
+
+                CustomTypeDeclaration type_ ->
+                    Just (Node.value type_.name)
+
+                PortDeclaration _ ->
+                    Nothing
+
+                InfixDeclaration _ ->
+                    Nothing
+
+                Destructuring _ _ ->
+                    Nothing
+        )
+        >> Set.fromList
 
 
 declarationVisitor : Node Declaration -> Context -> ( List (Rule.Error {}), Context )
@@ -88,13 +127,24 @@ declarationVisitor node context =
             doFunction function context
 
         AliasDeclaration typeAlias ->
-            ä
+            doTypeAnnotation typeAlias.typeAnnotation context
 
         CustomTypeDeclaration type_ ->
-            ä
+            type_.constructors
+                |> List.foldl
+                    (\constructor acc ->
+                        (Node.value constructor).arguments
+                            |> List.foldl
+                                (\arg ( accErrors, accContext ) ->
+                                    doTypeAnnotation arg accContext
+                                        |> Tuple.mapFirst (\newErrors -> accErrors ++ newErrors)
+                                )
+                                acc
+                    )
+                    ( [], context )
 
         PortDeclaration signature ->
-            ä
+            doTypeAnnotation signature.typeAnnotation context
 
         InfixDeclaration _ ->
             ( [], context )
@@ -113,21 +163,17 @@ letDeclarationEnterVisitor _ letDeclaration context =
             ( [], context )
 
 
-finalEvaluation : Context -> List (Rule.Error {})
-finalEvaluation context =
-    Debug.todo "declarationVisitor"
-
-
+doFunction : Function -> Context -> ( List (Rule.Error {}), Context )
 doFunction function context =
     case function.signature of
         Just (Node _ signature) ->
-            doTypeAnnotation signature.typeAnnotation
-                |> ä
+            doTypeAnnotation signature.typeAnnotation context
 
         Nothing ->
             ( [], context )
 
 
+doTypeAnnotation : Node TypeAnnotation -> Context -> ( List (Rule.Error {}), Context )
 doTypeAnnotation typeAnnotation context =
     case Node.value typeAnnotation of
         GenericType _ ->
@@ -135,14 +181,126 @@ doTypeAnnotation typeAnnotation context =
 
         Typed constructor arguments ->
             let
-                foo =
+                errors =
                     case constructor of
-                        Node _ ( [ qualifier ], name ) ->
+                        Node range ( [ qualifier ], name ) ->
                             if qualifier == name then
-                                -- Also check lookup table:
-                                -- Need to know which import to check and maybe update
-                                -- Need to look up the type name to see that it's free
-                                [ Rule.error "Redundantly qualified type" typeAnnotation ]
+                                if Set.member name context.typesDefinedInModule then
+                                    []
+
+                                else
+                                    case ModuleNameLookupTable.moduleNameFor context.lookupTable constructor of
+                                        Just moduleName ->
+                                            let
+                                                ( matchingImports, otherImports ) =
+                                                    context.imports
+                                                        |> partition
+                                                            (\import_ ->
+                                                                Node.value (Node.value import_).moduleName == moduleName
+                                                            )
+
+                                                otherImportConflicts =
+                                                    otherImports
+                                                        |> List.any
+                                                            (\importNode ->
+                                                                case (Node.value importNode).exposingList of
+                                                                    Just (Node _ exposing_) ->
+                                                                        case exposing_ of
+                                                                            Exposing.All _ ->
+                                                                                True
+
+                                                                            Exposing.Explicit nodes ->
+                                                                                nodes
+                                                                                    |> List.any
+                                                                                        (\topLevelExposeNode ->
+                                                                                            case Node.value topLevelExposeNode of
+                                                                                                Exposing.FunctionExpose _ ->
+                                                                                                    False
+
+                                                                                                Exposing.InfixExpose _ ->
+                                                                                                    False
+
+                                                                                                Exposing.TypeOrAliasExpose exposedName ->
+                                                                                                    exposedName == name
+
+                                                                                                Exposing.TypeExpose exposedType ->
+                                                                                                    exposedType.name == name
+                                                                                        )
+
+                                                                    _ ->
+                                                                        False
+                                                            )
+                                            in
+                                            if otherImportConflicts then
+                                                []
+
+                                            else
+                                                let
+                                                    exposesAll =
+                                                        matchingImports
+                                                            |> List.any
+                                                                (\importNode ->
+                                                                    case (Node.value importNode).exposingList of
+                                                                        Just (Node _ (Exposing.All _)) ->
+                                                                            True
+
+                                                                        _ ->
+                                                                            False
+                                                                )
+
+                                                    importFix =
+                                                        if exposesAll then
+                                                            []
+
+                                                        else
+                                                            let
+                                                                maybeExistingExposing =
+                                                                    matchingImports
+                                                                        |> List.filterMap
+                                                                            (\importNode ->
+                                                                                case (Node.value importNode).exposingList of
+                                                                                    Just (Node _ (Exposing.Explicit (firstExposed :: _))) ->
+                                                                                        Just firstExposed
+
+                                                                                    _ ->
+                                                                                        Nothing
+                                                                            )
+                                                                        |> List.head
+                                                            in
+                                                            case maybeExistingExposing of
+                                                                Just existingExposing ->
+                                                                    [ Fix.insertAt (Node.range existingExposing).start (name ++ ", ") ]
+
+                                                                Nothing ->
+                                                                    case matchingImports of
+                                                                        -- If no matching imports, assume it is part of the default imports.
+                                                                        [] ->
+                                                                            []
+
+                                                                        firstImport :: _ ->
+                                                                            [ Fix.insertAt (Node.range firstImport).end (" exposing (" ++ name ++ ")") ]
+                                                in
+                                                [ Rule.errorWithFix
+                                                    { message = "This type can be simplified to just `" ++ name ++ "`."
+                                                    , details = [ "It can be considered a bit silly to say the same word twice like in `" ++ name ++ "." ++ name ++ "`. This rule simplifies to just `" ++ name ++ "`. This follows the convention of centering modules around a type." ]
+                                                    }
+                                                    range
+                                                    (Fix.removeRange
+                                                        { start = range.start
+                                                        , end =
+                                                            { row = range.start.row
+
+                                                            -- Add 1 to the column to remove the dot.
+                                                            , column = range.start.column + String.length name + 1
+                                                            }
+                                                        }
+                                                        :: importFix
+                                                    )
+                                                ]
+
+                                        -- Should not happen.
+                                        Nothing ->
+                                            []
 
                             else
                                 []
@@ -150,29 +308,61 @@ doTypeAnnotation typeAnnotation context =
                         _ ->
                             []
             in
-            -- Now visit the arguments
-            -- Add test for `List (Set.Set a)`
-            ä
+            arguments
+                |> List.foldl
+                    (\arg ( accErrors, accContext ) ->
+                        doTypeAnnotation arg accContext
+                            |> Tuple.mapFirst (\newErrors -> accErrors ++ newErrors)
+                    )
+                    ( errors, context )
 
         Unit ->
             ( [], context )
 
         Tupled arguments ->
-            -- Now visit the arguments
-            -- Add tests for tuples and triples
-            ä
+            arguments
+                |> List.foldl
+                    (\arg ( accErrors, accContext ) ->
+                        doTypeAnnotation arg accContext
+                            |> Tuple.mapFirst (\newErrors -> accErrors ++ newErrors)
+                    )
+                    ( [], context )
 
         Record recordDefinition ->
-            -- Visit the things in the record
-            -- Add test for records
-            ä
+            doRecordDefinition recordDefinition context
 
         GenericRecord _ (Node _ recordDefinition) ->
-            -- Visit the things in the record
-            -- Add test for generic records
-            ä
+            doRecordDefinition recordDefinition context
 
         FunctionTypeAnnotation left right ->
-            -- Visit the left and right
-            -- Add test for function types
-            ä
+            let
+                ( newErrors, newContext ) =
+                    doTypeAnnotation left context
+            in
+            doTypeAnnotation right newContext
+                |> Tuple.mapFirst (\rightErrors -> newErrors ++ rightErrors)
+
+
+doRecordDefinition : List (Node RecordField) -> Context -> ( List (Rule.Error {}), Context )
+doRecordDefinition recordDefinition context =
+    recordDefinition
+        |> List.foldl
+            (\(Node _ ( _, typeAnnotation_ )) ( accErrors, accContext ) ->
+                doTypeAnnotation typeAnnotation_ accContext
+                    |> Tuple.mapFirst (\newErrors -> accErrors ++ newErrors)
+            )
+            ( [], context )
+
+
+partition : (a -> Bool) -> List a -> ( List a, List a )
+partition predicate list =
+    List.foldr
+        (\element ( trueList, falseList ) ->
+            if predicate element then
+                ( element :: trueList, falseList )
+
+            else
+                ( trueList, element :: falseList )
+        )
+        ( [], [] )
+        list
